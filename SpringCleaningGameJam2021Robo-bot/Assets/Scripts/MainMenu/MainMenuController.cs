e83b5ecf457d739c612b85825159dc6c220921f6
DanielChambi <daniel.chambi.campos@alumnos.upm.es>
2021-03-24 01:14:36 +0100

    Added Main Menu Scene. Added provisional Scenes for current game genres. Added transition to first game scene.

---
+++ SpringCleaningGameJam2021Robo-bot/Assets/Scripts/MainMenu/MainMenuController.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+public class MainMenuController : MonoBehaviour
+{
+    public Button startButton;
+
+    string firstGameScene = "Scenes/BulletHell/BulletHellScene";
+
+    void Start()
+    {
+        startButton.onClick.AddListener(StartGame);    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void StartGame()
+    {
+        StartCoroutine(LoadFirstGameScene());   
+    }
+
+    IEnumerator LoadFirstGameScene()
+    {
+        AsyncOperation Asyncload = SceneManager.LoadSceneAsync(firstGameScene, LoadSceneMode.Single);
+
+        while( !Asyncload.isDone)
+        {
+            yield return null;
+        }
+    }
+}
