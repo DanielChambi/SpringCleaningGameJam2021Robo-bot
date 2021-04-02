35db3afbef1d5e1a157a8f40aa381b04df01c5aa
DanielChambi <daniel.chambi.campos@alumnos.upm.es>
2021-04-02 20:34:28 +0200

    Added boss shoot phase movement

---
+++ SpringCleaningGameJam2021Robo-bot/Assets/Scripts/BullletHell/Enemies/BHEnemyBoss.cs
@@ -0,0 +1,107 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BHEnemyBoss : MonoBehaviour
+{
+    [SerializeField]
+    private Transform[] routes;
+
+    private int routeToGo;
+
+    private float tParam;
+
+    private Vector2 objectPosition;
+
+    private float speedModifier;
+
+    private bool routeCoroutineAllowed;
+
+
+    public bool followRoute;
+
+    BossPhase phase;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        routeToGo = 0;
+        tParam = 0f;
+        speedModifier = 1f;
+        routeCoroutineAllowed = true;
+
+        followRoute = true;
+        phase = BossPhase.ShootPhase;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        switch (phase)
+        {
+            case BossPhase.ShootPhase:
+                if (routeCoroutineAllowed)
+                {
+                    StartCoroutine(GoByTheRoute(routeToGo));
+                }
+                break;
+            case BossPhase.VacuumPhase:
+                break;
+        }
+
+
+    }
+
+    void UpdateShootPhase()
+    {
+
+    }
+
+    void UpdateVacuumPhase()
+    {
+
+    }
+
+    private IEnumerator GoByTheRoute(int routeNum)
+    {
+        routeCoroutineAllowed = false;
+
+        Vector2 p0 = routes[routeNum].GetChild(0).position;
+        Vector2 p1 = routes[routeNum].GetChild(1).position;
+        Vector2 p2 = routes[routeNum].GetChild(2).position;
+        Vector2 p3 = routes[routeNum].GetChild(3).position;
+
+        while (tParam < 1)
+        {
+            if (followRoute)
+            {
+                tParam += Time.deltaTime * speedModifier;
+            }
+
+            objectPosition = Mathf.Pow(1 - tParam, 3) * p0 + 3 * Mathf.Pow(1 - tParam, 2) * tParam * p1 + 3 * (1 - tParam) * Mathf.Pow(tParam, 2) * p2 + Mathf.Pow(tParam, 3) * p3;
+
+            transform.position = objectPosition;
+            yield return new WaitForEndOfFrame();
+        }
+
+        tParam = 0f;
+
+        routeToGo += 1;
+
+        if (routeToGo > routes.Length - 1)
+        {
+            routeToGo = 0;
+        }
+
+        routeCoroutineAllowed = true;
+
+    }
+
+    enum BossPhase
+    {
+        Null,
+        ShootPhase,
+        VacuumPhase
+    }
+}
