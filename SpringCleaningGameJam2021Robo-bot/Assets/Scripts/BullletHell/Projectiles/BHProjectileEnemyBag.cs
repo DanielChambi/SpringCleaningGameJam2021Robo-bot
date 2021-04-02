f8918594ba73aa70f72eabb4caa4130e77b8d019
DanielChambi <daniel.chambi.campos@alumnos.upm.es>
2021-04-02 20:56:44 +0200

    Added garbage bag projectile

---
+++ SpringCleaningGameJam2021Robo-bot/Assets/Scripts/BullletHell/Projectiles/BHProjectileEnemyBag.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BHProjectileEnemyBag : BHProjectileEnemy
+{
+    float explodeTime;
+    float explodeTimeMin = 1.75f;
+    float explodeTimeMax = 2f;
+    float explodeTimer = 0;
+
+    float speed = 5;
+
+    public GameObject circlePattern;
+
+    private void Start()
+    {
+        explodeTime = Random.Range(explodeTimeMin, explodeTimeMax);
+    }
+    protected override void ProjectileMovement()
+    {
+        explodeTimer += Time.deltaTime;
+        if(explodeTimer >= explodeTime)
+        {
+            Instantiate(circlePattern, transform.position, Quaternion.identity);
+            Destroy(gameObject);
+        }
+
+        transform.Translate(Vector2.down * speed * Time.deltaTime, Space.World);
+    }
+}
